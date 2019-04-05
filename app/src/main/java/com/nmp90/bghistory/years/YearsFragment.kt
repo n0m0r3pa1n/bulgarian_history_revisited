5d7e7dfee73ff373cb356107644a1020f9a8f308
Georgi Mirchev <gmirchev90@gmail.com>
2019-04-05 21:49:01 +0300

    White text for SearchView

--- app/src/main/java/com/nmp90/bghistory/years/YearsFragment.kt
+++ app/src/main/java/com/nmp90/bghistory/years/YearsFragment.kt
@@ -2,7 +2,9 @@ package com.nmp90.bghistory.years
 
 import android.os.Bundle
 import android.view.*
+import android.widget.EditText
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.nmp90.bghistory.R
 import com.nmp90.reactivelivedata2.subscribeSingle
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class YearsFragment : Fragment() {
 
@@ -39,9 +42,12 @@ class YearsFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        val searchItem = menu.findItem(R.id.action_search)
+        val searchItem = menu.findItem(com.nmp90.bghistory.R.id.action_search)
         searchItem.isVisible = true
         val searchView: SearchView = searchItem.actionView as SearchView
+        val searchEditText = searchView.findViewById(androidx.appcompat.R.id.search_src_text) as EditText
+        searchEditText.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+        searchEditText.setHintTextColor(ContextCompat.getColor(requireContext(), R.color.white))
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 searchEvents(query ?: "")
