a4f6a9c5f9869558e9eee8121c4daa7e86e3a53c
Georgi Mirchev <gmirchev90@gmail.com>
2019-04-07 10:34:04 +0300

    Bug fixes for activity rotation

--- app/src/main/java/com/nmp90/bghistory/capitalDetails/CapitalDetailsActivity.kt
+++ app/src/main/java/com/nmp90/bghistory/capitalDetails/CapitalDetailsActivity.kt
@@ -8,6 +8,35 @@ import androidx.appcompat.app.AppCompatActivity
 import com.nmp90.bghistory.R
 
 class CapitalDetailsActivity : AppCompatActivity() {
+
+    private lateinit var capitalId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events)
+        capitalId = savedInstanceState?.getString(ARG_CAPITAL_ID) ?: intent.getStringExtra(ARG_CAPITAL_ID)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, CapitalDetailsFragment.newInstance(capitalId))
+            .commit()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(ARG_CAPITAL_ID, capitalId)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     companion object {
 
         private val ARG_CAPITAL_ID = "capital"
@@ -17,29 +46,5 @@ class CapitalDetailsActivity : AppCompatActivity() {
             intent.putExtra(ARG_CAPITAL_ID, capitalId)
             return intent
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_events)
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container,
-                CapitalDetailsFragment.newInstance(
-                    intent.getStringExtra(ARG_CAPITAL_ID)
-                )
-            )
-            .commit()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == android.R.id.home) {
-            this.finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/nmp90/bghistory/events/EventsActivity.kt
+++ app/src/main/java/com/nmp90/bghistory/events/EventsActivity.kt
@@ -10,6 +10,37 @@ import com.nmp90.bghistory.topics.Topic
 
 class EventsActivity : AppCompatActivity() {
 
+    private var topicId: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events)
+
+        topicId = savedInstanceState?.getInt(ARG_TOPIC_ID) ?: intent.getIntExtra(ARG_TOPIC_ID, 0)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, EventsFragment.newInstance(topicId))
+                .commit()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(ARG_TOPIC_ID, topicId)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     companion object {
 
         private val ARG_TOPIC_ID = "topic"
@@ -19,32 +50,5 @@ class EventsActivity : AppCompatActivity() {
             intent.putExtra(ARG_TOPIC_ID, topic.id)
             return intent
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_events)
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container,
-                EventsFragment.newInstance(
-                    intent.getIntExtra(
-                        ARG_TOPIC_ID,
-                        0
-                    )
-                )
-            )
-            .commit()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == android.R.id.home) {
-            this.finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
